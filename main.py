92c80d2dab2f86128b2547d61051585ee5696741
Vlad-home <vlad-home@iMac-Vlad-home.local>
2020-12-13 14:06:22 +0300

    Введеный текст отправляется в окно вывода

--- main.py
+++ main.py
@@ -14,11 +14,29 @@ class Application(QWidget):
     def __init__(self):
         super().__init__()
         self.init_UI()
+        self.event_hundler()
     
 
     def init_UI(self):
         self.ui = uic.loadUi("./desing.ui")
         self.ui.show()
+    
+
+    def event_hundler(self):
+        
+        self.event_btn()
+    
+    
+    def event_btn(self):
+        self.ui.BtnSendMessage.clicked.connect(self.send_message)
+    
+
+    def send_message(self, event):
+        text_message = self.ui.TextInputMessage.toPlainText().rstrip()
+        if text_message != '':
+            self.ui.TextInputMessage.clear()
+            self.ui.Messages.append(text_message)
+        
 
 
 
